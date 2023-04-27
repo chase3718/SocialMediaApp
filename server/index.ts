2f857a1627079e7e9922fad35e0be241d7a9528c
Chase Williams <chasewilliams98@gmail.com>
2023-04-27 17:23:20 -0400

    Added mysql to server

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,28 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+const mysql = require('mysql');
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM users', (err: any, rows: any) => {
+  if (err) throw err;
+
+  console.log('Data received from Db:');
+  console.table(rows);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
