8923cb05251e4788b748107fae102e51d93fb839
Chase Williams <chasewilliams98@gmail.com>
2023-04-28 17:06:26 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import get_handler from './get_handler';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.all('/metadata', (req: Request, res: Response) => {
-  let response;
-	if (req.method === 'GET') {
-    console.log('its a get request');
-		response = get_handler(req, res);
-	}
-});
+
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
