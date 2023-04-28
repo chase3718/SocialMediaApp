e7635f279e5d065a22e94c81857e0af69a5c3e90
Chase Williams <chasewilliams98@gmail.com>
2023-04-27 22:15:54 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
-const mysql = require('mysql');
+import get_handler from './get_handler';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect();
-
-connection.query('SELECT * FROM users', (err: any, rows: any) => {
-  if (err) throw err;
-
-  console.log('Data received from Db:');
-  console.table(rows);
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.all('/metadata', (req: Request, res: Response) => {
+  let response;
+	if (req.method === 'GET') {
+    console.log('its a get request');
+		response = get_handler(req, res);
+	}
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
